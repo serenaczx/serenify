851a6820d0b1cd260aec880e86bb71c8f9f57330
xuanzhi33 <xuanzhi33@qq.com>
2024-05-27 14:08:26 +0800

    feat: 信息搜集

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import webview
 import subprocess
 from dirscan import scan_website
+from info import search_info
 
 
 window = webview.create_window('Serenify', './src/ui/home.html')
-window.expose(scan_website)
+window.expose(scan_website, search_info)
 webview.start(debug=True)
